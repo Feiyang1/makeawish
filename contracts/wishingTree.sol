7045b1d4fcc3ce4d2390b53d8fe17861b805d3d2
Feiyang Chen <plane1113@gmail.com>
2018-02-03 16:00:58 -0500

    allow owner to withdraw

--- contracts/wishingTree.sol
+++ contracts/wishingTree.sol
@@ -5,7 +5,8 @@ contract WishingTree {
     uint price = 1; // unit ether
     address public owner = msg.sender;
     
-    function WishingTree() public {}modifier onlyBy(address _account) {require(msg.sender == _account);_;}
+    function WishingTree() public {}
+    modifier onlyBy(address _account) {require(msg.sender == _account);_;}
     
     function addMessage(uint idx, string message) payable public {
         if(msg.value >= price) {
@@ -21,29 +22,9 @@ contract WishingTree {
         return messages[idx];
     }
     
-    function readMessages() public {}
-    
-    // event Transaction(string _type, address indexed user);
-    // function StockExchange() {
-        // holdings = 10000;
-        // }
-        // function buy() payable {
-            // if(msg.value == 1 ether) {
-                // balances[msg.sender] += 1;
-                // holdings -= 1;
-                // Transaction(\"Buy321\",msg.sender);
-                // }
-                // }
-                // function sell() {
-                    // balances[msg.sender] -= 1;
-                    // holdings += 1;
-                    // msg.sender.transfer(1 ether);
-                    // Transaction(\"Sell\",msg.sender);
-                    // }
-                    // function getBalance(address addr) returns(uint) {
-                        // return balances[addr];
-                        // }
-                        // function getHoldings() returns(uint) {
-                            // return holdings;
-                            // }
+    function withdraw() public 
+    onlyBy(owner) {
+        owner.transfer(this.balance);
+    }
+
 }
