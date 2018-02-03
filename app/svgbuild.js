e5053de62d15772066829e3ceeb214f221d4171b
Feiyang Chen <plane1113@gmail.com>
2018-02-03 16:01:10 -0500

    remove ui code

--- app/svgbuild.js
+++
@@ -1,9 +0,0 @@
-var svgstore = require('svgstore');
-var fs = require('fs');
-
-var sprites = svgstore()
-    .add('tree1', fs.readFileSync('./resources/noun_441084_cc.svg', 'utf8'))
-    .add('tree3', fs.readFileSync('./resources/noun_441096_cc.svg', 'utf8'))
-    .add('tree2', fs.readFileSync('./resources/noun_441093_cc.svg', 'utf8'));
-
-fs.writeFileSync('./resources/sprites.svg', sprites);
